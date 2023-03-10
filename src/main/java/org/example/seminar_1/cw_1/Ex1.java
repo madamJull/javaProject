087f9b0526e81eaabfe9d2185b857ec63c3f108f
Juliya <9123288799@mail/ru>
2023-03-10 22:51:18 +0500

    Correct

--- src/main/java/org/example/seminar_1/cw_1/Ex1.java
+++ src/main/java/org/example/seminar_1/cw_1/Ex1.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class Ex1 {
     public static void main(String[] args) {
-        int sum = 0, multi = 1;
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        System.out.println(subtractProductAndSum(n));
+    }
+
+    public static int subtractProductAndSum(int n) {
+        int sum = 0, multi = 1;
         while (n != 0) {
             sum += n % 10;
             multi *= n % 10;
             n /= 10;
         }
-        System.out.println("result = " + (multi - sum));
+        return multi - sum;
     }
 }
