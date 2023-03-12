d54eabf4f6f11e2a1d0e91707c5afaebcf3f335e
Juliya <9123288799@mail/ru>
2023-03-12 22:15:14 +0500

    Add Ex2

---
+++ src/main/java/org/example/seminar_1/cw_2/Ex2.java
@@ -0,0 +1,23 @@
+package org.example.seminar_1.cw_2;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args){
+        //Найти количество положительных чисел, после которых следует отрицательное
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = 0;
+        int a = sc.nextInt();
+        for (int i = 1; i < n; i++) {
+            int b = sc.nextInt();
+            if (a > 0 && b < 0){
+                k++;
+            }
+            a = b;
+        }
+        System.out.println("k : "+k);
+        sc.close();
+    }
+
+}
